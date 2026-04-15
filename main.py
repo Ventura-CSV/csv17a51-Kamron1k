2e5f03db798e4baae65a205dbc464863e83c9942
Kamron1k <your-github-email@example.com>
2026-04-14 23:36:41 -0700

    final autograder fix

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-student = {'name': 'Kamron1k', 'id': '900954459', 'course': 'CSV17', 'grade': 'A'}
+student = {
+    'name': 'Kamron1k',
+    'id': '900954459',
+    'course': 'CSV17',
+    'grade': 'A'
+}
 
 h = {10: 'x', 20: 'y', 30: 'x', 40: 'z'}
+
 domain = set(h.keys())
 range_vals = set(h.values())
 
